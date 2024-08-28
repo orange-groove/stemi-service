86108b39ccdcfada03f0afa1c18a7ac3b3e876fc
Adam Groves <adam.groves333@gmail.com>
2024-08-27 21:12:29 -0400

    up

--- flaskr/app.py
+++ flaskr/app.py
@@ -16,4 +16,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, port=os.getenv('PORT', 5000))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
